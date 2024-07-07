c6ea881293080b40cf824e7848dade4299ec182b
Moin Shadab <56497815+Moin-shadab@users.noreply.github.com>
2024-07-07 20:46:02 +0530

    Add files via upload

---
+++ invoicepdf.php
@@ -0,0 +1,48 @@
+<?php
+error_reporting(E_ALL ^ E_DEPRECATED);
+
+require_once('tcpdf/tcpdf.php');
+
+// create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Your Company');
+$pdf->SetTitle('Invoice');
+$pdf->SetSubject('Invoice PDF');
+$pdf->SetKeywords('TCPDF, PDF, invoice');
+
+// set default header data
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
+
+// set header and footer fonts
+$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// add a page
+$pdf->AddPage();
+
+// define some HTML content with style
+$html = file_get_contents('invoice.html');
+
+// output the HTML content
+$pdf->writeHTML($html, true, false, true, false, '');
+
+//Close and output PDF document
+$pdf->Output('invoice.pdf', 'I');
+?>
